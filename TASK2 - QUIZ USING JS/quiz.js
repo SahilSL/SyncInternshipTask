cdd51b4024da718fd4dbef167fface3005c38268
SahilSL <108080446+SahilSL@users.noreply.github.com>
2023-02-18 21:51:07 +0530

    Add files via upload

---
+++ TASK2 - QUIZ USING JS/quiz.js
@@ -0,0 +1,107 @@
+let quiz_rules = [
+    "You have to solve given Question within a time limit given to you.",
+    "Once you select any anwer. It can't be undone",
+    "You'll get marks on the basic of your correct answers.",
+    "There is no negative marking scheme"
+];
+
+
+let quiz_questions = [
+    // question 
+    {
+        question: "HTML stands for : ?",
+
+        options : [
+            "Hyper Text Markup Language",
+            "Hyperlinks and Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyper Text Making Language",
+        ],
+
+        // correct answer
+        answer: 1,
+    },
+    
+    {
+        question: "Internet Explorer is a: ",
+    
+        options: [
+            "Any person browsing the net",
+            "Web Browser",
+            "Graphing Package",
+            "News Reader",
+        ],
+        answer: 2,
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+    
+        options: [
+            "script",
+            "scripting",
+            "js",
+            "javascript",
+        ],
+        answer: 1,
+
+    },
+    {
+        question: "What does SQL stand for?",
+    
+        options: [
+            "Structure Query Language",
+            "Strong Question Language",
+            "Structured Question Language",
+        ],
+        answer: 1,
+    },
+    {
+        question: "What does PHP stand for?",
+    
+        options: [
+            "PHP: Hypertext Preprocessor",
+            "Personal Hypertext Processor",
+            "Private Homepage",
+        ],
+        answer: 1,
+    },
+    {
+        question: "Which of the following operating systems is produced by IBM?",
+    
+        options: [
+            "OS-2",
+            "Windows",
+            "DOS",
+            "UNIX",
+        ],
+
+        answer: 1,
+    },
+    {
+
+        question: "Bootstrap is framework of CSS.",
+    
+        options: [
+            "False",
+            "True",
+        ],
+        
+        answer: 2,
+
+    },
+    {
+        
+        question: "Which of the following is the correct extension of the Python file?",
+    
+        options: [
+            '.python',
+            '.pl',
+            '.py',
+            '.p',
+        ],
+        
+        answer: 3,
+    
+    },
+    
+];
